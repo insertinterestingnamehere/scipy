f9fc3114ae1b9468169b964f3031e50c04ac751b
Pauli Virtanen <pav@iki.fi>
2015-01-15 14:43:27 +0200

    ENH: Changed the Cython api for BLAS and LAPACK to link directly against compiled fortran instead of relying on f2py.

--- scipy/linalg/__init__.py
+++ scipy/linalg/__init__.py
@@ -138,16 +138,6 @@ Special Matrices
    toeplitz - Toeplitz matrix
    tri - Construct a matrix filled with ones at and below a given diagonal
 
-
-Utility Functions
-=================
-
-.. autosummary::
-   :toctree: generated/
-
-   get_linalg_dir - Get the directory where scipy.linalg is installed
-
-
 Low-level routines
 ==================
 
@@ -164,9 +154,9 @@ Low-level routines
 
    `scipy.linalg.lapack` -- Low-level LAPACK functions
 
-   `scipy.linalg.blas_pointers` -- Function pointers to BLAS functions
+   `scipy.linalg.cython_blas` -- Low-level BLAS functions for Cython
 
-   `scipy.linalg.lapack_pointers` -- Function pointers to LAPACK functions
+   `scipy.linalg.cython_lapack` -- Low-level LAPACK functions for Cython
 
 """
 
@@ -175,7 +165,6 @@ from __future__ import division, print_function, absolute_import
 from .linalg_version import linalg_version as __version__
 
 from .misc import *
-from .utils import *
 from .basic import *
 from .decomp import *
 from .decomp_lu import *
